bf495379a7a83ee883094c3b4ee44a88545109a3
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-03-13 21:59:01 -0500

    first commit

---
+++ Main.py
@@ -0,0 +1,12 @@
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+
+print(scipy.__version__)
+print(numpy.__version__)
+print(matplotlib.__version__)
+print(pandas.__version__)
+print(sklearn.__version__)
+
